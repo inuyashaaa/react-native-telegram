524a84bc8b79254b10a44649079b78ce362e39d8
Vương Đỗ <vuong.gtvt1@gmail.com>
2020-08-25 10:22:50 +0700

    add exercise

---
+++ vuongdovan/assets/images/index.js
@@ -0,0 +1,11 @@
+const back = require('./back.png');
+const profile = require('./profile.png');
+const setting1 = require('./union.png');
+const setting2 = require('./group-5.png');
+const setting3 = require('./group-6.png');
+const setting4 = require('./chat.png');
+const setting5 = require('./rectangle-55.png');
+
+export {
+    back, profile, setting1, setting2, setting3, setting4, setting5
+}
